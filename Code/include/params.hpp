d809b5408062203454992f045d7a9f4d2f634e55
invrecon <invrecon@protonmail.com>
2023-06-29 11:24:45 +0200

    Set desired defaults

--- Code/include/params.hpp
+++ Code/include/params.hpp
@@ -43,7 +43,7 @@ class Params
 		title="Enemy Spawn Distance";
 		values[]={500,800,1200};
 		texts[]={"Short (better performance, spawn in view possible)", "Medium", "Far (for good rigs)"};
-		default = 800;
+		default = 1200;
 	};
 	class A3E_Param_VillageSpawnCount
 	{
@@ -58,7 +58,7 @@ class Params
 		title="Search Chopper Type";
 		values[]={0,1,2};
 		texts[]={"Random (Chooses between Easy and Hard Values)","Easy","Hard"};
-		default = 0;
+		default = 1;
 	};
 	class A3E_Param_Spacer2
     {
@@ -67,22 +67,22 @@ class Params
             texts[] = {""};
             default = 0;
 	};
-	class A3E_Param_TimeOfDay	
-	{	
+	class A3E_Param_TimeOfDay
+	{
 	    title="Time Of Day";
 		values[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
 		texts[]={"00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","Random","Daytime","Nighttime"};
-		default = 8;
+		default = 15;
 		//function = "A3E_fnc_paramDaytime"; // (Optional) Function called when player joins, selected value is passed as an argument
 	};
-	class A3E_Param_TimeMultiplier	
-	{	
+	class A3E_Param_TimeMultiplier
+	{
 	    title="Time Multiplier (Fasttime)";
 		values[]={1,6,12,24,36};
 		texts[]={"1:1 (Normal)","1:6 (Day = 4 Hours)","1:12 (Day = 2 Hours)","1:24 (Day = 1 Hour)","1:36 (Day = 40 Minutes)"};
 		livechanges = 1;
 		code = "if(isserver) then {setTimeMultiplier _this;};";
-		default = 6;
+		default = 1;
 	};
 	class A3E_Param_WeatherOvercast
 	{
@@ -96,7 +96,7 @@ class Params
 		title="Fog Density";
 		values[] = {-1,0,1,2,3,4,5,6,7,8,9,10};
 		texts[] = {"Randomly picks one of the below values (heavily weighted towards no fog for a better gameplay experience)","No Fog","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
-		default = -1;
+		default = 0;
 	};
 	class A3E_Param_WeatherWind
 	{
@@ -113,7 +113,7 @@ class Params
 		default = -1;
 	};
 	class A3E_Param_Grass
-	{	
+	{
 		title="Grass Visibility";
 		values[]={50,25,12,6,2};
 		texts[]={"No Grass", "Proximity", "Normal", "Far", "Very Far"};
@@ -131,7 +131,7 @@ class Params
 		title="Collect intel to reveal markers on map";
 		values[]={0,1};
 		texts[]={"Disabled","Enabled"};
-		default = 1;
+		default = 0;
 		tooltip = "This will enable the collection of intel from enemies. Intel will reveal locations on the map.";
 	};
 	class A3E_Param_IntelChance
@@ -147,7 +147,7 @@ class Params
 		title="Display of markers on map";
 		values[]={0,1,2,3};
 		texts[]={"Always show (with type)","Always show marker but hide type (questionmark)","Show marker upon discovery","Never show markers"};
-		default = 2;
+		default = 0;
 	};
 	class A3E_Param_VehicleLock
 	{
@@ -184,7 +184,7 @@ class Params
 		values[]={0, 1, 2};
 		texts[]={"Random", "Close", "Far"};
 		livechanges = 1;
-		default = 0;
+		default = 1;
 	};
 	class A3E_Param_Waffelbox
 	{
@@ -283,7 +283,7 @@ class Params
 		title="Mag repack";
 		values[]={0,1};
 		texts[]={"Disabled", "Enabled"};
-		default = 0;
+		default = 1;
 	};
 	class A3E_Param_Spacer7
     {
